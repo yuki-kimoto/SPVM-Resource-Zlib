e91421abd2545a946a8865d23ec9012e4f400d86
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-12-21 14:21:25 +0900

    fix typo

--- lib/SPVM/Resource/Zlib/V1_2.pm
+++ lib/SPVM/Resource/Zlib/V1_2.pm
@@ -8,5 +8,5 @@ SPVM::Resource::Zlib::V1_2 - zlib v1.2 Resources
 
 =head1 DESCRIPTION
 
-SPVM::Resource::Zlib::V1_2 provide zlib 1.2.x resources. Curent zlib version is 1.2.11.
+SPVM::Resource::Zlib::V1_2 provides zlib 1.2.x resources. Current zlib version is 1.2.11.
 
