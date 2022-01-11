93ac5fc3cae50708f467703a3332d423fa5654a8
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-01-11 13:45:36 +0900

    Fix config file and include zlib.h

--- lib/SPVM/Resource/Zlib/V1_2.c
+++ lib/SPVM/Resource/Zlib/V1_2.c
@@ -1,10 +1,6 @@
 #include "spvm_native.h"
 
-#include <math.h>
-#include <fenv.h>
-#include <complex.h>
-
-static const char* MFILE = "SPVM/Math.c";
+#include <zlib.h>
 
 int32_t SPVM__Resource__Zlib__V1_2__dummy(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
